692223c6e4a223b2f99114cc097cdb5a597ae1a9
Kuba <xkubaxmx@gmail.com>
2023-12-12 15:24:20 +0100

    first method bodies implementations

--- bi_ring.h
+++ bi_ring.h
@@ -24,18 +24,44 @@ public:
         friend class bi_ring<Key, Info>;
 
     public:
-        iterator();
-        iterator(const iterator &src);
-        ~iterator();
+        iterator() { owner = nullptr;}
+        iterator(const iterator &src)
+        {
+            owner = src.owner;
+        }
+        ~iterator() {};
 
-        iterator &operator=(const iterator &src);
+        iterator &operator=(const iterator &src)
+        {
+            owner = src.owner;
+            return *this;
+        }
 
-        bool operator==(const iterator &other) const;
-        bool operator!=(const iterator &other) const;
+        bool operator==(const iterator &other) const
+        {
+            return owner == other.owner;
+        }
+        bool operator!=(const iterator &other) const
+        {
+            return owner != other.owner;
+        }
 
-        iterator &operator++();
-        iterator operator++(int);
-        iterator &operator--();
+        iterator &operator++()
+        {
+            owner = owner->next;
+            return *this;
+        }
+        iterator operator++(int)
+        {
+            iterator temp = *this;
+            owner = owner->next;
+            return temp;
+        }
+        iterator &operator--()
+        {
+            owner = owner->previous;
+            return *this;
+        }
         iterator operator--(int);
         iterator operator+(int n) const;
         iterator operator-(int n) const;
@@ -54,7 +80,7 @@ public:
         friend class bi_ring<Key, Info>;
 
     public:
-        const_iterator();
+        const_iterator() { owner = nullptr;}
         const_iterator(const const_iterator &src);
         ~const_iterator();
 
@@ -76,8 +102,11 @@ public:
         const Node *operator->() const;
     };
 
-    bi_ring();
-    bi_ring(const bi_ring &src);
+    bi_ring(){ start = nullptr; size = 0;};
+    bi_ring(const bi_ring &src)
+    {
+        
+    }
     ~bi_ring();
 
     bi_ring &operator=(const bi_ring &src);
@@ -88,9 +117,55 @@ public:
     int size() const { return size; }
     bool empty() { return size == 0; }
 
-    iterator push_front(const Key &key, const Info &info);
-    iterator pop_front();
-    iterator insert(iterator position, const Key &key, const Info &info); // inserts after the given position
+    iterator push_front(const Key &key, const Info &info)
+    {
+        if (start == nullptr)
+        {
+            start = new Node;
+            start->key = key;
+            start->info = info;
+            start->next = start;
+            start->previous = start;
+        }
+        else
+        {
+            Node *newNode = new Node;
+            newNode->key = key;
+            newNode->info = info;
+            newNode->next = start;
+            newNode->previous = start->previous;
+            start->previous->next = newNode;
+            start->previous = newNode;
+            start = newNode;
+        }
+        return iterator(start);
+    }
+    iterator pop_front()
+    {
+        if (start == nullptr)
+        {
+            return iterator();
+        }
+        else if (start->next == start)
+        {
+            delete start;
+            start = nullptr;
+            return iterator();
+        }
+        else
+        {
+            Node *temp = start;
+            start->previous->next = start->next;
+            start->next->previous = start->previous;
+            start = start->next;
+            delete temp;
+            return iterator(start);
+        }
+    }
+    iterator insert(iterator position, const Key &key, const Info &info) // inserts after the given position
+    {
+
+    }
     iterator erase(iterator position);
     void clear();
 
