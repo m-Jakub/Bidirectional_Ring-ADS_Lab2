ce68b762b44f9056c52a8679e50be041b6ffab61
Kuba <xkubaxmx@gmail.com>
2023-12-12 08:42:28 +0100

    more methods

--- bi_ring.h
+++ bi_ring.h
@@ -1,7 +1,7 @@
 
 
 template <typename Key, typename Info>
-class bi_ring // implemented as doubly linked list
+class Bi_ring // implemented as doubly linked list
 {
 private:
     struct Node
@@ -12,28 +12,69 @@ private:
         Node *previous;
     };
     Node *start; // front
+    int size;
 
 public:
-    class iterator
+    class Iterator
+    {
+    private:
+        Node *owner;
+        Iterator(Node *start);
+
+        friend class Bi_ring<Key, Info>;
+
+    public:
+
+        Iterator();
+        Iterator(const Iterator &src);
+        ~Iterator();
+
+        Iterator &operator=(const Iterator &src);
+
+        bool operator==(const Iterator &other) const;
+        bool operator!=(const Iterator &other) const;
+
+        Iterator &operator++();
+        Iterator operator++(int);
+        Iterator &operator--();
+        Iterator operator--(int);
+        Iterator operator+(int n) const;
+        Iterator operator-(int n) const;
+        Iterator &operator+=(int n);
+        Iterator &operator-=(int n);
+
+        Key &operator*() const;
+        Key *operator->() const;
+
+        bool empty() const { return owner == nullptr; }
+
+
+
+    };
+    class Const_iterator
     { /* … */
     };
-    class const_iterator
-    { /* … */
-    };
-    bi_ring();
-    bi_ring(const bi_ring &src);
-    ~bi_ring();
-    bi_ring &operator=(const bi_ring &src);
-    iterator push_front(const Key &key, const Info &info);
-    iterator pop_front();
-    iterator insertAtPos(iterator position, const Key &key, const Info &info);
-    iterator eraseAtPos(iterator position);
-    iterator searchKey(const Key &key);
-    // bool addAtPos(const Key &key, const Info &info, int which);
-    // bool removeByKey(const Key &key, int which);
-    // bool getInfo(const Key &key, const Info &info, int which);
-    Iterator search(const Key &key, const Info &info, int which);
-    int getSize();
-    // what else can be useful in such a collection?
-    // use examples of additional functions to guide you in the interface design
+
+    Bi_ring();
+    Bi_ring(const Bi_ring &src);
+    ~Bi_ring();
+
+    Bi_ring &operator=(const Bi_ring &src);
+
+    bool getInfo(Iterator position, Info &info);
+    Iterator search(const Key &key, int which);
+
+    int getSize() { return size; }
+    bool empty() { return size == 0; }
+
+    Iterator push_front(const Key &key, const Info &info);
+    Iterator pop_front();
+    Iterator insert(Iterator position, const Key &key, const Info &info); // inserts after the given position
+    Iterator erase(Iterator position);
+    void clear();
+
+    void print();
+    friend ostream &operator<<(ostream &os, const Ring<Key, Info> &ringToPrint);
+
+    Iterator begin() { return Iterator(start); }
 };
